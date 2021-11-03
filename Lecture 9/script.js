7f926d296cfbf140f623cb4118a89b76d56950c5
Neru-chan <91089733+Neru-chan@users.noreply.github.com>
2021-11-03 16:14:05 +0200

    Update script.js

--- Lecture 9/script.js
+++ Lecture 9/script.js
@@ -1,10 +1,66 @@
-function User (name)
+// 1
+
+function A(name)
 {
     this.name = name;
-    this.isAdmin = false;
 }
 
-let user = new User("Вася");
+function B(name)
+{
+    this.name = name;
+}
 
-console.log(user.name);
-console.log(user.isAdmin);
+let a = new A("Вася");
+let b = new B("Вася");
+
+console.log( a == b ); //false
+
+let obj = {
+    name: "Вася"
+}
+
+function C()
+{
+    return obj;
+}
+
+function D()
+{
+    return obj;
+}
+
+let c = new C;
+let d = new D;
+
+console.log( c == d ); //true
+
+console.log("=====");
+
+// 2
+
+function Calc()
+{
+    let a;
+    let b;
+
+    this.read = function() {
+        this.a = Number(prompt(a, 0));
+        this.b = Number(prompt(b, 0));
+    }
+
+    this.sum = function() {
+        return this.a + this.b;
+    };
+
+    this.mul = function() {
+        return this.a * this.b;
+    };
+}
+
+let calc = new Calc();
+
+calc.read();
+
+console.log("SUM of " + calc.a + " and " + calc.b + " = " + calc.sum());
+
+console.log("MUL of " + calc.a + " and " + calc.b + " = " + calc.mul());
